2df001e4609cc16aaa3f8e52a21b3555883f8c28
AshishRanjan-004 <ashish.patel@zorang.com>
2020-02-16 21:10:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const parser = require("body-parser");
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(parser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(parser.json());
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to the API"
+  });
+});
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "Post created...",
+        authData
+      });
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  user = req.body;
+  console.log(user);
+  jwt.sign({ user }, "secretkey", (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
+
+app.listen(5000, () => console.log("Server started on port 5000"));
